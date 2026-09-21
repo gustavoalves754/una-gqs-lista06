617c4c7f1b1fa9786c827dd2ed86246a4dece692
gustavo.alves <gustavohm754@gmail.com>
2026-09-21 19:49:54 -0300

    Primeiro commit: Adicionado o algoritimo

---
+++ teste.js
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Olá, Git!");
+
+        String nome = "João";
+        int idade = 20;
+
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+    }
+}
